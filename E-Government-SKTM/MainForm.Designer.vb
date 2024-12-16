ecd29931ec6cec39ed8fb7dd349b53b9bbf56c81
Agung Firdaus <agung.firdaus@kemenkeu.go.id>
2024-12-16 12:42:22 +0700

    Add project files.

---
+++ E-Government-SKTM/MainForm.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNik = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.lstApplications = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.MenuStrip2.SuspendLayout()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNik
+        '
+        Me.txtNik.Location = New System.Drawing.Point(217, 89)
+        Me.txtNik.Name = "txtNik"
+        Me.txtNik.Size = New System.Drawing.Size(461, 20)
+        Me.txtNik.TabIndex = 0
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(217, 149)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(461, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(217, 222)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(461, 20)
+        Me.txtAddress.TabIndex = 2
+        '
+        'lstApplications
+        '
+        Me.lstApplications.FormattingEnabled = True
+        Me.lstApplications.Location = New System.Drawing.Point(220, 280)
+        Me.lstApplications.Name = "lstApplications"
+        Me.lstApplications.Size = New System.Drawing.Size(457, 95)
+        Me.lstApplications.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(251, 400)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(102, 35)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(359, 400)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(102, 35)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(467, 400)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(102, 35)
+        Me.btnPrint.TabIndex = 6
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(575, 400)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(102, 35)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 8
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip2.TabIndex = 9
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'menuExit
+        '
+        Me.menuExit.Name = "menuExit"
+        Me.menuExit.Size = New System.Drawing.Size(93, 22)
+        Me.menuExit.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuAbout})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'menuAbout
+        '
+        Me.menuAbout.Name = "menuAbout"
+        Me.menuAbout.Size = New System.Drawing.Size(107, 22)
+        Me.menuAbout.Text = "About"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "NIK"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "NAMA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 225)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "ALAMAT"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(200, 24)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(477, 24)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Sistem Pengajuan Surat Keterangan Tidak Mampu"
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstApplications)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtNik)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Form1"
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNik As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents lstApplications As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents menuExit As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents menuAbout As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+End Class
---
+++ E-Government-SKTM/MainForm.vb
@@ -0,0 +1,113 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Public Class MainForm
+    Inherits Form
+
+    ' Enumeration for message types
+    Private Enum MessageType
+        Info
+        Warning
+        [Error]
+    End Enum
+
+    ' List to store application data
+    Private applicationData As New List(Of String)()
+
+    ' Constant message
+    Private Const AppTitle As String = "E-Government Application - Surat Keterangan Tidak Mampu"
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = AppTitle
+    End Sub
+
+    ' Event: Add Button Click
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim nik As String = txtNIK.Text.Trim()
+        Dim name As String = txtName.Text.Trim()
+        Dim address As String = txtAddress.Text.Trim()
+
+        If String.IsNullOrEmpty(nik) OrElse String.IsNullOrEmpty(name) OrElse String.IsNullOrEmpty(address) Then
+            ShowMessage("Please fill in all fields!", MessageType.Warning)
+            Return
+        End If
+
+        Dim data As String = $"NIK: {nik}, Name: {name}, Address: {address}, Type: Surat Keterangan Tidak Mampu"
+        applicationData.Add(data)
+
+        ' Update ListBox
+        lstApplications.Items.Add(data)
+
+        ShowMessage("Application submitted successfully!", MessageType.Info)
+
+        ' Clear input fields
+        ClearInputs()
+    End Sub
+
+    ' Event: Clear Button Click
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearInputs()
+    End Sub
+
+    ' Event: Print Button Click
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        If lstApplications.SelectedIndex = -1 Then
+            ShowMessage("Please select an application to print!", MessageType.Warning)
+            Return
+        End If
+
+        Dim selectedApplication As String = lstApplications.SelectedItem.ToString()
+
+        Dim result As DialogResult = MessageBox.Show($"Do you want to print this application?\n\n{selectedApplication}",
+            AppTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.Yes Then
+            ShowMessage("Application printed successfully!", MessageType.Info)
+        End If
+    End Sub
+
+    ' Event: Exit Button Click
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    ' Menu: File -> Exit
+    Private Sub menuExit_Click(sender As Object, e As EventArgs) Handles menuExit.Click
+        Application.Exit()
+    End Sub
+
+    ' Menu: Help -> About
+    Private Sub menuAbout_Click(sender As Object, e As EventArgs) Handles menuAbout.Click
+        MessageBox.Show("E-Government App - Surat Keterangan Tidak Mampu\nVersion 1.0\nDeveloped by [Your Name]", AppTitle, MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    ' Method to clear input fields
+    Private Sub ClearInputs()
+        txtNIK.Clear()
+        txtName.Clear()
+        txtAddress.Clear()
+    End Sub
+
+    ' Method to show a message box
+    Private Sub ShowMessage(message As String, type As MessageType)
+        Dim icon As MessageBoxIcon = MessageBoxIcon.Information
+
+        If type = MessageType.Warning Then
+            icon = MessageBoxIcon.Warning
+        ElseIf type = MessageType.Error Then
+            icon = MessageBoxIcon.Error
+        End If
+
+        MessageBox.Show(message, AppTitle, MessageBoxButtons.OK, icon)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles menuExit.Click
+        Application.Exit()
+    End Sub
+
+End Class
